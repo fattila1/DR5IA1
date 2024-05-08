4f0fd4a0869eb9f5e49bc361a547b6ae13e35601
fattila1 <150517040+fattila1@users.noreply.github.com>
2024-05-08 11:18:46 +0200

    Update variables.tf

--- Modules/files/variables.tf
+++ Modules/files/variables.tf
@@ -1,1 +1,14 @@
+variable "Name" {
+  description = "The name of player"
+  type        = string
+}
 
+variable "Number" {
+  description = "The number of player"
+  type        = string
+}
+
+variable "Team" {
+  description = "The team's name, where the player is playing"
+  type        = string
+}
