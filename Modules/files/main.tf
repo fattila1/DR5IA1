e60a739e0c70dbc021a59bff6791fc6e62d4936e
fattila1 <150517040+fattila1@users.noreply.github.com>
2024-05-08 11:50:17 +0200

    Update main.tf

--- Modules/files/main.tf
+++ Modules/files/main.tf
@@ -1,1 +1,10 @@
-
+module "players"
+{
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "~> 5.0"
+  project_id  = "ekke-DR5IA1"
+  names = ["ekke-cloud"]
+  prefix = "our"
+  set_admin_roles = true
+  admins = ["user:simark2357@gmail.com"]
+}
